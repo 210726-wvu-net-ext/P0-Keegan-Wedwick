438156a6d4ae383c1035a3246971660dacc3ff05
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-07-29 12:07:34 -0500

    added the arithmatic.sh file to shell scripting folder

---
+++ Shell_Scripting/arithmatic.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+read -p "Enter your grade: " grade
+if (( "$grade" <= 40 && "$grade" >= 0 ))
+then
+    echo "Your grade is an F."
+elif (( "$grade" > 40 && "$grade" <= 50 ))
+then
+    echo "Your grade is a D."
+elif (( "$grade" > 50 && "$grade" <= 60 ))
+then
+    echo "Your grade is a C."
+elif (( "$grade" > 60 && "$grade" <= 70 ))
+then
+    echo "Your grade is a B."
+elif (( "$grade" > 70 && "$grade" <= 100 ))
+then
+    echo "Your grade is an A."
+else
+    echo "You've entered an invalid grade"
+fi
