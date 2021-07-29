ff3adb935fd974080f6ed7633f6f1d12b17b0d6c
kwedwick <64446581+kwedwick@users.noreply.github.com>
2021-07-29 12:40:26 -0500

    added even/odd checker

--- Shell_Scripting/arithmatic.sh
+++ Shell_Scripting/arithmatic.sh
@@ -19,3 +19,13 @@ then
 else
     echo "You've entered an invalid grade"
 fi
+
+
+
+read -p "Enter a number: " num1
+if [ $((num1%2)) = 0 ]
+then
+    echo "$num1 is an even number."
+else
+    echo "$num1 is an odd number."
+fi
